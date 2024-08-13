773747a04579a5eaf3a2b7e1c304a276cb5bd475
kweonsikyung <ekrk6632@naver.com>
2024-08-13 10:17:32 +0900

    set: build init setting

--- src/main/java/demo/demo/DemoApplication.java
+++ src/main/java/demo/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package demo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DemoApplication {
 
@@ -11,3 +13,4 @@ public class DemoApplication {
     }
 
 }
+햐
